ce311d0607f192c3ecec0effcb6132b6be808169
DONGMIN LEE <movemin03@naver.com>
2025-10-23 09:22:06 +0900

    Add configuration for Supabase and IMGBB API

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+window.ENV = {
+    SUPABASE_URL: 'https://ipcibnhhegkhdwyxeqjs.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwY2libmhoZWdraGR3eXhlcWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MzMyNzYsImV4cCI6MjA3NjMwOTI3Nn0.LKjw1dqgjs39shvGpxcfFPutA7p1yR9eOb4zhomSBS8',
+    IMGBB_API_KEY: '4a0d69744fa47d8de59028ec369b7abc'
+};
